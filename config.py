4664e8b5ee2eaac2449914500e129eaccc822b6b
hyperfraise <damien.menigaux@polytechnique.edu>
2019-01-31 11:30:15 +0100

    shorter steps

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ parser.add_argument(
     "--TICK", type=int, default=1000
 )  # 1 tick = 1000 images = (1000/batch_size) iter.
 parser.add_argument("--max_resl", type=int, default=8)  # 10-->1024, 9-->512, 8-->256
-parser.add_argument("--trns_tick", type=int, default=200)  # transition tick
-parser.add_argument("--stab_tick", type=int, default=100)  # stabilization tick
+parser.add_argument("--trns_tick", type=int, default=100)  # transition tick
+parser.add_argument("--stab_tick", type=int, default=50)  # stabilization tick
 
 
 ## network structure.
